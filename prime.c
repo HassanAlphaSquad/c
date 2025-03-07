0283808b6eede0569fd2e13c783ed12ea6759262
HassanAlphaSquad <hassanzahid.alphasquad@gmail.com>
2025-03-07 23:53:53 +0500

    prime numbers (WiP)

--- prime.c
+++ prime.c
@@ -4,9 +4,15 @@ int print_primes(int max)
 {
     for (int i = 2; i <= max; i++)
     {
-        for (int j = 2; j <= i; j++)
+        for (int j = 2; j <= max; j++)
         {
-            printf("")
+            // printf("%d=>%d\n",i,j);
+            if (i%j!=0){
+                if (i==j){
+                    break;
+                }
+                printf("%d=>%d\n",i,j);
+            }
             // if (j%i!=0){
             //     if (i==j){
             //         continue;
